f76649ad0281650cc979f6f98bc740893518cafa
paboowicz <paboowicz@gmail.com>
2022-09-22 08:26:40 +0200

    nowy element slidera na stronie glownej w wersji en i de

--- en/index.php
+++ en/index.php
@@ -19,6 +19,9 @@
                             <a href="labels48h.php" class="slide">
                                 <img src="./static/image/slider/slider_onas_03.jpg" alt="test">
                             </a>
+							<a href="labels48h.php" class="slide">
+                                <img src="./static/image/slider/slider_onas_04.jpg" alt="test">
+                            </a>
                         </div>
                     </div>
                 </div>
